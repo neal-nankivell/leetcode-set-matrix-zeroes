493aeb3e91c3f5541257103e21d4a538d2426060
neal.nankivell <neal.nankivell@gmail.com>
2019-09-08 10:36:46 +0200

    Implmented solution to 0 our rows and columns in 2d array if the row/column contains a zero
    note: this problem requested the matrix be modified
    in place on leetcode.
    
    This performs two passes over the matrix in each direction
    generating lists of row/column indexes that need to be zeroed
    out. A bit of room for improvement here as we could probally
    generate these lists in a single pass.
    
    Then we do a single pass over the rows in the matrix,
    updating them all to zeros by copying in an empty row.
    Or make a pass over the columns that need to be zeroed
    out.

--- solution.py
+++ solution.py
@@ -1,3 +1,22 @@
+from typing import List
+'''
+Given a m x n matrix, if an element is 0, set its entire row and column to 0.
+Do it in-place.
+'''
+
+
 class Solution:
-    def foo(self) -> str:
-        return "bar"
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rows = [i for i, row in enumerate(matrix) if 0 in row]
+        cols = [i for i, col in enumerate(zip(*matrix)) if 0 in col]
+
+        emptyRow = [0] * len(matrix[0])
+        for i in range(len(matrix)):
+            if i in rows:
+                matrix[i] = emptyRow
+            else:
+                for j in cols:
+                    matrix[i][j] = 0
--- test_solution.py
+++ test_solution.py
@@ -1,13 +1,44 @@
 import solution
 import unittest
 
+'''
+Example 1:
+
+Input: 
+[
+  [1,1,1],
+  [1,0,1],
+  [1,1,1]
+]
+Output: 
+[
+  [1,0,1],
+  [0,0,0],
+  [1,0,1]
+]
+'''
+
 
 class Test_TestSolution(unittest.TestCase):
     def setUp(self):
         self.sut = solution.Solution()
 
-    def test_foo(self):
-        self.assertEqual(self.sut.foo(), "bar")
+    def test_setZeroes(self):
+        input = [
+            [1, 1, 1],
+            [1, 0, 1],
+            [1, 1, 1]
+        ]
+
+        expectedOutput = [
+            [1, 0, 1],
+            [0, 0, 0],
+            [1, 0, 1]
+        ]
+
+        self.sut.setZeroes(input)
+
+        self.assertEqual(input, expectedOutput)
 
 
 if __name__ == '__main__':
